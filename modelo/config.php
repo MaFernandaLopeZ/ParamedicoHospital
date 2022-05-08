bbc749c93b1a456175006ade8bcce554cad2827e
MaFernandaLopeZ <maferlz265@gmail.com>
2022-05-07 23:02:17 -0500

    mod datos

--- modelo/config.php
+++ modelo/config.php
@@ -6,6 +6,7 @@ $pass = "";
 $database = "uiu";
 
 $conexion = mysqli_connect($server, $user, $pass, $database);
+// $conection=new mysqli("localhost:3306", "promay20_artea","restaurante1234","promay20_restauranteartea");
 
 if(!$conexion) {
     die("<script>alert('Conexion fallida')</script>");
